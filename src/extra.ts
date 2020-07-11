d97c953cfa740d2618c03f8e3939b78e06ae17b2
Cotton Hou <himcotton@gmail.com>
2020-07-11 23:48:35 +0800

    the extra.cjs

---
+++ src/extra.ts
@@ -0,0 +1,8 @@
+export * from './utils';
+export { logger, logLevel, errToIgnoresBy } from './model';
+
+export { netConnectTo } from './servers/index';
+export { cryptoPairs, cryptoPairsC } from './servers/shadowsocks';
+export { socks5Proxy } from './services/socks5';
+export { ShadowSocks } from './settings/utils';
+
