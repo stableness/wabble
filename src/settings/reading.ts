307f8744e47915ef9b2019da15349159447d7bec
Cotton Hou <himcotton@gmail.com>
2020-10-04 07:28:17 +0800

    refine settings/reading.ts

--- src/settings/reading.ts
+++ src/settings/reading.ts
@@ -1,15 +1,19 @@
 import { URL } from 'url';
-import { AssertionError } from 'assert';
 
 import * as R from 'ramda';
 
-import { eq as Eq, option as O, function as F } from 'fp-ts';
+import {
+    eq as Eq,
+    either as E,
+    option as O,
+    function as F,
+} from 'fp-ts';
 
 import * as Dc from 'io-ts/Decoder';
 
 import type { Config, Basic, Remote } from '../config';
 
-import { Fn, readOptionalString, portNormalize, readTrimmedNonEmptyString } from '../utils';
+import * as u from '../utils';
 
 import { ShadowSocks, Trojan } from './utils';
 
@@ -17,16 +21,46 @@ import { ShadowSocks, Trojan } from './utils';
 
 
 
-export function convert (obj: unknown): Config {
+const baseURI = Dc.type({ uri: Dc.string });
 
-    assertObject(obj);
 
-    const raw = obj as Record<'services' | 'doh' | 'servers' | 'rules' | 'tags', unknown>;
 
-    assertBaseArray(raw.servers);
-    assertBaseArray(raw.services);
 
-    const services = R.map(({ uri }) => {
+
+export const CF_DOH_ENDPOINT = 'https://cloudflare-dns.com/dns-query' as HttpOrHttps;
+
+export type HttpOrHttps = string & { readonly HttpOrHttps: unique symbol };
+
+export const trimmedStartsWithHttpOrHttps = F.pipe(
+    u.readTrimmedNonEmptyString,
+    Dc.refine(
+        R.either(
+            R.startsWith('http://'),
+            R.startsWith('https://'),
+        ) as (str: string) => str is HttpOrHttps,
+        'HttpOrHttps',
+    ),
+);
+
+const flagOnToCloudFlare = F.pipe(
+    Dc.boolean,
+    Dc.parse(on => on ? Dc.success(CF_DOH_ENDPOINT) : Dc.failure(on, 'off')),
+);
+
+export const decodeDoH = Dc.union(
+    flagOnToCloudFlare,
+    trimmedStartsWithHttpOrHttps,
+);
+
+
+
+
+
+const decodeServices = F.pipe(
+
+    baseURI,
+
+    Dc.parse(({ uri }) => {
 
         const { protocol, port, hostname, username, password } = new URL(uri);
         const proto = R.init(protocol);
@@ -46,28 +80,47 @@ export function convert (obj: unknown): Config {
         if (proto === 'socks5' || proto === 'http') {
             if (hostname.length > 0) {
 
-                return {
+                return Dc.success({
                     auth,
                     port: R.subtract(+port, +(process.env.DEV_PORT_MINUS || 0)),
                     host: hostname,
                     protocol: proto as typeof proto,
-                };
+                });
 
             }
         }
 
-        fail('invalid service');
+        return Dc.failure(uri, 'invalid service');
 
-    }, raw.services);
+    }),
 
-    const servers = filterTags(raw.tags, raw.servers.map(server => {
+    u.decoderNonEmptyArrayOf,
 
-        const { uri, tags } = { tags: [], ...server };
+);
+
+
+
+
+
+const decodeServers = F.pipe(
+
+    baseURI,
+
+    Dc.intersect(Dc.partial({
+        tags: u.readTrimmedNonEmptyStringArr,
+        key: u.readTrimmedNonEmptyString,
+        password: u.readTrimmedNonEmptyString,
+        ssl: Dc.UnknownRecord,
+    })),
+
+    Dc.parse(server => {
+
+        const { uri, tags = [] } = server;
 
         const url = new URL(uri);
 
         const { protocol, hostname, username, password } = url;
-        const port = portNormalize(url);
+        const port = u.portNormalize(url);
         const proto = R.init(protocol);
 
         const baseWith = R.mergeLeft({
@@ -76,12 +129,14 @@ export function convert (obj: unknown): Config {
             tags: new Set([ ...tags, proto ]),
         });
 
+        let result;
+
         if (proto === 'ss') {
 
             const config = ShadowSocks.parse(server);
 
             if (config) {
-                return baseWith({ ...config, protocol: proto } as const);
+                result = baseWith({ ...config, protocol: proto } as const);
             }
 
         }
@@ -91,7 +146,7 @@ export function convert (obj: unknown): Config {
             const config = Trojan.parse(server);
 
             if (config) {
-                return baseWith({ ...config, protocol: proto } as const);
+                result = baseWith({ ...config, protocol: proto } as const);
             }
 
         }
@@ -103,7 +158,7 @@ export function convert (obj: unknown): Config {
                 : O.some({ username, password })
             ;
 
-            return baseWith({ protocol: proto, auth } as const);
+            result = baseWith({ protocol: proto, auth } as const);
 
         }
 
@@ -116,63 +171,91 @@ export function convert (obj: unknown): Config {
                 : O.some(R.join(':', [ username, password ]))
             ;
 
-            return baseWith({ protocol: proto, ssl: { verify }, auth } as const);
+            result = baseWith({ protocol: proto, ssl: { verify }, auth } as const);
 
         }
 
-        fail('invalid server');
+        if (result != null) {
+            return Dc.success(result);
+        }
 
-    }));
+        return Dc.failure(uri, 'invalid server');
 
-    const rules = (function (raw: unknown) {
+    }),
 
-        type Rules = Partial<Config['rules']>;
-        const obj = R.defaultTo({}, raw) as Rules;
+    u.decoderNonEmptyArrayOf,
 
-        const { direct = [], reject = [], proxy = [] } = obj;
-
-        return { direct, reject, proxy };
-
-    }(raw.rules));
-
-    const sieve = {
-        direct: readOptionalString(R.path([ 'sieve', 'direct' ], raw)),
-        reject: readOptionalString(R.path([ 'sieve', 'reject' ], raw)),
-    };
-
-    const doh = O.fromEither(decodeDoH.decode(raw.doh));
-
-    return { services, doh, servers, rules, sieve } as const;
-
-}
+);
 
 
 
 
 
-export const CF_DOH_ENDPOINT = 'https://cloudflare-dns.com/dns-query' as HttpOrHttps;
+const decodeRules = Dc.type({
+    proxy: u.readTrimmedNonEmptyStringArr,
+    direct: u.readTrimmedNonEmptyStringArr,
+    reject: u.readTrimmedNonEmptyStringArr,
+});
 
-export type HttpOrHttps = string & { readonly HttpOrHttps: unique symbol };
 
-export const trimmedStartsWithHttpOrHttps = F.pipe(
-    readTrimmedNonEmptyString,
-    Dc.refine(
-        R.either(
-            R.startsWith('http://'),
-            R.startsWith('https://'),
-        ) as (str: string) => str is HttpOrHttps,
-        'HttpOrHttps',
+
+
+
+export const { decode: decodeConfig } = F.pipe(
+
+    Dc.type({
+
+        servers: decodeServers,
+        services: decodeServices,
+        rules: decodeRules,
+
+    }),
+
+    Dc.intersect(Dc.partial({
+
+        doh: decodeDoH,
+        tags: u.readTrimmedNonEmptyStringArr,
+
+        sieve: Dc.partial({
+            direct: u.readTrimmedNonEmptyString,
+            reject: u.readTrimmedNonEmptyString,
+        }),
+
+    })),
+
+);
+
+
+
+
+
+export const convert: u.Fn<unknown, Config> = F.flow(
+
+    decodeConfig,
+
+    E.mapLeft(Dc.draw),
+
+    E.map(({ services, doh, servers, rules, tags, sieve }) => ({
+
+        rules,
+        services,
+
+        servers: filterTags(tags, servers),
+
+        doh: O.fromNullable(doh),
+
+        sieve: {
+            direct: O.fromNullable(sieve?.direct),
+            reject: O.fromNullable(sieve?.reject),
+        },
+
+    } as const)),
+
+    E.fold(
+        e => { throw e; },
+        F.identity,
     ),
-);
 
-export const flagOnToCloudFlare = F.pipe(
-    Dc.boolean,
-    Dc.parse(on => on ? Dc.success(CF_DOH_ENDPOINT) : Dc.failure(on, 'off')),
-);
-
-export const decodeDoH = Dc.union(
-    flagOnToCloudFlare,
-    trimmedStartsWithHttpOrHttps,
 );
 
 
@@ -197,13 +280,13 @@ export function filterTags <T extends TagsOnlyRemote> (tags: unknown, servers: T
     const i = R.reject(R.startsWith('-'), t);
 
     // Exclude
-    const e = R.map(R.tail as Fn<string>, R.symmetricDifference(i, t));
+    const e = R.map(R.tail as u.Fn<string>, R.symmetricDifference(i, t));
 
     // tags to array
     const t2a = R.o(
         Array.from,
         R.prop('tags') as () => T['tags'],
-    ) as Fn<T, string[]>;
+    ) as u.Fn<T, string[]>;
 
     const isSubset = R.o(
         R.isEmpty,
@@ -221,63 +304,4 @@ export function filterTags <T extends TagsOnlyRemote> (tags: unknown, servers: T
 
 
 
-
-
-function fail (message = 'unknown'): never {
-    throw new AssertionError({ message });
-}
-
-
-
-
-
-function assertObject (obj: unknown): asserts obj is object {
-
-    if (obj == null) {
-        fail('null or undefined');
-    }
-
-    if (typeof obj !== 'object') {
-        fail('not object');
-    }
-
-}
-
-
-
-
-
-export function assertBaseArray (obj: unknown): asserts obj is Base[] {
-
-    if (!(obj instanceof Array) || obj == null) {
-        fail('not array');
-    }
-
-    if (obj.length < 1) {
-        fail('empty array');
-    }
-
-    for (const item of obj) {
-        assertBase(item);
-    }
-
-}
-
-
-
-
-
-interface Base {
-    uri: string;
-}
-
-function assertBase (obj: unknown): asserts obj is Base {
-
-    assertObject(obj);
-
-    if (R.not('uri' in obj)) {
-        fail('no uri');
-    }
-
-}
 
