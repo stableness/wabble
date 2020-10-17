a396cc5992cdd644eb7670ac1fc3d5e26c4abbeb
Cotton Hou <himcotton@gmail.com>
2020-10-18 07:28:19 +0800

    refine toServer

--- src/servers/index.ts
+++ src/servers/index.ts
@@ -5,6 +5,7 @@ import type { Logger } from 'pino';
 import * as R from 'ramda';
 
 import {
+    task as T,
     taskEither as TE,
     either as E,
     map as fpMap,
@@ -15,7 +16,7 @@ import {
 } from 'fp-ts';
 
 import type { Remote } from '../config';
-import { DoH, Fn, tryCatchToError } from '../utils';
+import { DoH, Fn, run, tryCatchToError } from '../utils';
 import { logLevel } from '../model';
 import type { Hook } from '../services/index';
 
@@ -59,69 +60,34 @@ const hostCache = R.memoizeWith(
 
 export function connect ({ host, port, hook, dns, doh, logger }: Opts) {
 
-    const ipCache = nsLookup(host);
     const justHost = hostCache(host);
-
     const mergeOpts = R.mergeRight({ port, logger, hook });
 
 
 
     return async function toServer (server: O.Option<Remote> | 'nothing') {
 
-        const ipOr = await F.pipe(
-            host,
-            O.fromPredicate(doh),
-            O.chain(F.constant(dns)),
-            O.ap(justHost),
-            O.map(task => F.pipe(
-                ipCache,
-                O.map(data => [ {
-                    data,
-                    name: host,
-                    TTL: 1,
-                    type: 1 as const,
-                } ]),
-                TE.fromOption(Error),
-                TE.alt(F.constant(task)),
-            )),
-            O.map(TE.map(F.flow(
-                A.findFirst(R.where({
-                    type: R.equals(1),
-                    data: R.is(String),
-                })),
-                O.map(R.tap(({ data, TTL }) => {
+        const please = doh(host) ? F.constUndefined : F.constant(host);
 
-                    if (O.isNone(ipCache)) {
-                        dnsCache.set(host, data);
-                        setTimeout(() => dnsCache.delete(host), TTL * 1000);
-                    }
+        const query = () => F.pipe(
 
-                })),
-                O.map(R.prop('data')),
-                O.map(R.tap(ip => {
+            O.chain (nsLookup) (justHost),
+            TE.fromOption(() => new Error('No cache')),
 
-                    if (R.not(logLevel.on.trace)) {
-                        return;
-                    }
+            TE.alt(F.constant(F.pipe(
 
-                    logger.child({ ip }).trace('DoH');
+                O.ap (justHost) (dns),
+                TE.fromOption(() => new Error('No DoH')),
+                TE.chain(parse),
 
-                })),
             ))),
-            O.map(R.applyTo(0)),
-            O.toNullable,
+
+            TE.getOrElseW(() => T.fromIO(F.constUndefined)),
+
         );
 
-
-
-        const ipOrHost = F.pipe(
-            ipOr,
-            E.fromNullable(Error('Non DoH')),
-            E.flatten,
-            O.fromEither,
-            O.flatten,
-            O.getOrElse(F.constant(host)),
-        );
+        //                        do                    by
+        const ipOrHost = please() ?? await run(query()) ?? host;
 
 
 
@@ -178,6 +144,46 @@ export function connect ({ host, port, hook, dns, doh, logger }: Opts) {
         );
 
     };
+
+
+
+    function parse (results: ReturnType<ReturnType<typeof DoH>>) {
+
+        return F.pipe(
+
+            results,
+
+            TE.map(F.flow(
+
+                A.findFirst(R.where({
+                    type: R.equals(1),
+                    data: R.is(String),
+                })),
+
+                O.map(R.tap(({ data: ip, TTL }) => {
+
+                    if (O.isNone(nsLookup(host))) {
+                        dnsCache.set(host, ip);
+                        setTimeout(() => dnsCache.delete(host), TTL * 1000);
+                    }
+
+                    if (R.not(logLevel.on.trace)) {
+                        return;
+                    }
+
+                    logger.child({ ip }).trace('DoH');
+
+                })),
+
+                O.map(R.prop('data')),
+
+            )),
+
+            TE.chain(TE.fromOption(() => new Error('No valid entries'))),
+
+        );
+
+    }
 
 }
 
