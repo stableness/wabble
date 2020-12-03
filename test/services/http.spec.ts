4308c06ca7c210ce6ae1c53e3ac1b3b77b2a3530
Cotton Hou <himcotton@gmail.com>
2020-12-04 01:12:15 +0800

    refine services/http.spec.ts

--- test/services/http.spec.ts
+++ test/services/http.spec.ts
@@ -1,22 +1,30 @@
 import net from 'net';
 import http from 'http';
-import { URL } from 'url';
 import { once } from 'events';
+import { URL, URLSearchParams as SearchParams } from 'url';
 
 import {
+    apply,
     option as O,
+    function as F,
     either as E,
     taskEither as TE,
-    function as F,
+    readerTaskEither as RTE,
+    stateReaderTaskEither as SRTE,
 } from 'fp-ts';
 
-import * as R from 'ramda';
-
+import * as Rx from 'rxjs';
 import * as o from 'rxjs/operators';
+
+import * as R from 'ramda';
 
 import Defer from 'p-defer';
 
 import pino from 'pino';
+
+import {
+    box,
+} from '../../src/services/index';
 
 import {
     httpProxy,
@@ -34,13 +42,31 @@ import type { Service, Basic } from '../../src/config';
 type Req = http.ClientRequest;
 type IcM = http.IncomingMessage;
 
+type Closing = { close: () => void };
+
+type Ports = Readonly<Record<'proxy' | 'server', number>>;
+
+export type Result <T> = SRTE.StateReaderTaskEither<Ports, Env, Error, T>;
+
+export type TE_ES = TE.TaskEither<Error, string>;
+
+export type Env = Readonly<{
+    url: URL;
+    logging: Logging;
+    flags: ReturnType<typeof genFlags>;
+}>;
+
+
+
+
+
+jest.retryTimes(0);
+
 
 
 
 
 describe('httpProxy', () => {
-
-    const { auth } = genAuth({ username: 'foo', password: 'bar' });
 
     test.each([
 
@@ -48,6 +74,8 @@ describe('httpProxy', () => {
         {  warn: true },
 
     ])('short for proxy auth in %p', async opts => {
+
+        const { auth } = genAuth({ username: 'foo', password: 'bar' });
 
         const service: Service = {
             auth,
@@ -64,7 +92,7 @@ describe('httpProxy', () => {
 
         const proxyPort = await p.promise;
 
-        const result = await u.run(fetch(
+        const result = await u.run(fetchToString(
 
             flushHeaders(
                 http.request({
@@ -100,7 +128,7 @@ describe('httpProxy', () => {
 
         const proxyPort = await p.promise;
 
-        const result = await u.run(fetch(
+        const result = await u.run(fetchToString(
 
             flushHeaders(
                 http.request({
@@ -129,107 +157,68 @@ describe('httpProxy', () => {
 
     test.each([
 
-        'http://   foo:bar   @   localhost:7000   /hello   ?   c=world',
-        'http://                 localhost:7100   /hello   ?   c=world',
-        'http://                 localhost:7100   /hello   ?   c=world & e=1',
+        'http://   foo:bar   @   localhost   /hello   ?   c=world             ',
+        'http://  -foo:bar   @   localhost   /hello   ?   c=world  & a & d    ',
+        'http://                 localhost   /hello   ?   c=world             ',
+        'http://                 localhost   /hello   ?   c=world- & e        ',
 
     ])('%s', async raw => {
 
         const url = new URL(R.replace(/ /g, '', raw));
+        const flags = genFlags(url.searchParams);
+        const logging = genLogging({ debug: flags.d });
 
-        const search = queryFrom(url);
-        const { auth, headers } = genAuth(url);
-        const server = genServer(url);
+        const env: Env = { url, flags, logging };
 
-        const service: Service = {
-            auth,
-            protocol: 'http',
-            host: url.hostname,
-            port: 0,
-        };
+        const invoke = SRTE.evaluate({ proxy: 0, server: 0 });
 
-        const p = Defer<number>();
+        await RTE.run(F.pipe(
 
-        const proxy = httpProxy (service) (genLogging(), p.resolve);
+            invoke(sequence({
+                server: temp.server,
+                proxy: temp.proxy,
+                REQUEST: temp.REQUEST,
+                CONNECT: temp.CONNECT,
+            })),
 
-        const sub = proxy.pipe(
-            o.mergeMap(({ host, port, hook }) => {
+            RTE.map(({ proxy, server, REQUEST, CONNECT }) => ({
+                task: TE.sequenceArray([ REQUEST, CONNECT ]),
+                close () {
+                    proxy.close();
+                    server.close();
+                },
+            })),
 
-                if (search('e') === '1') {
-                    return hook();
-                }
+            RTE.chain(({ close, task }) => RTE.bracket(
+                RTE.of({}),
+                F.constant(RTE.fromTaskEither(task)),
+                F.constant(RTE.fromIO(close)),
+            )),
 
-                return hook(net.connect({ host, port }));
+            RTE.fold(
 
-            }),
-        ).subscribe(u.noop);
+                error => RTE.fromIO(() => {
 
-        const proxyPort = await p.promise;
+                    if (flags.a || flags.e) {
+                        return;
+                    }
 
-        REQUEST: {
+                    expect(error).toBeUndefined();
 
-            const result = await u.run(fetch(
-
-                flushHeaders(
-                    http.request({
-                        headers,
-                        host: service.host,
-                        port: proxyPort,
-                        method: 'GET',
-                        path: redirect(url),
-                    }),
-                ),
-
-            ));
-
-            if (search('e') === '1') {
-                expect(E.isLeft(result)).toBe(true);
-                break REQUEST;
-            }
-
-            expect(result).toStrictEqual(E.right(search('c')));
-
-        }
-
-        CONNECT: {
-
-            const req = flushHeaders(
-                http.request({
-                    headers,
-                    host: service.host,
-                    port: proxyPort,
-                    method: 'CONNECT',
-                    path: R.join(':', [ url.hostname, url.port ]),
                 }),
-            );
 
-            const result = await u.run(F.pipe(
+                ([ request, connect ]) => RTE.fromIO(() => {
 
-                u.tryCatchToError(() => once(req, 'connect')),
+                    const content = readStr(flags.c ?? '');
 
-                TE.map(R.nth(1)),
+                    expect(request).toBe(content);
+                    expect(connect).toBe(content);
 
-                TE.chain((socket: net.Socket) => fetch(
-                    flushHeaders(
-                        http.request(redirect(url), {
-                            createConnection: F.constant(socket),
-                        }),
-                    ),
-                )),
+                }),
 
-            ));
+            ),
 
-            if (search('e') === '1') {
-                expect(E.isLeft(result)).toBe(true);
-                break CONNECT;
-            }
-
-            expect(result).toStrictEqual(E.right(search('c')));
-
-        }
-
-        sub.unsubscribe();
-        server.close();
+        ), env);
 
     });
 
@@ -239,15 +228,23 @@ describe('httpProxy', () => {
 
 
 
-const flushHeaders = R.tap((req: Req) => req.flushHeaders());
+export const sequence = apply.sequenceS(SRTE.stateReaderTaskEither);
 
-const queryFrom =
+export const flushHeaders = R.tap((req: Req) => req.flushHeaders());
+
+export const queryFrom =
     (url: URL) =>
         (key: string): string | null =>
             url.searchParams.get(key)
 ;
 
-function fetch (req: Req) {
+export const readStr: u.Fn<string> = R.cond([
+    [ R.startsWith('-'), R.tail ],
+    [   R.endsWith('-'), R.init ],
+    [ R.T, R.identity ],
+]);
+
+export function fetchToString (req: Req) {
 
     return u.tryCatchToError(async () => {
 
@@ -265,12 +262,14 @@ function fetch (req: Req) {
 
 }
 
-function genAuth ({ username, password }: Basic) {
+export function genAuth ({ username, password }: Basic) {
 
     const auth = F.pipe(
         O.some(u.eqBasic({ username, password })),
         O.filter(test => test({ username: '', password: '' }) === false),
     );
+
+    const basic = O.chain (F.constant(O.some({ username, password }))) (auth);
 
     const hasAuth = u.option2B(auth);
 
@@ -282,33 +281,27 @@ function genAuth ({ username, password }: Basic) {
         ...(hasAuth && { 'Proxy-Authorization': credentials } ),
     };
 
-    return { auth, headers };
+    return { auth, basic, headers };
 
 }
 
-function redirect ({ hostname, port, pathname }: URL) {
-    return `http://${ hostname }:${ port }${ pathname }`;
+export function redirect (url: URL, optionPort?: number) {
+    const { hostname, port, pathname } = url;
+    return `http://${ hostname }:${ optionPort ?? port }${ pathname }`;
 }
 
-function genServer (url: URL) {
+export function genFlags (search: SearchParams) {
 
-    const search = queryFrom(url);
+    const a = search.has('a');
+    const e = search.has('e');
+    const d = search.has('d');
+    const c = search.get('c');
 
-    return http.createServer((req, res) => {
-
-        if (req.method === 'GET' && req.url === url.pathname) {
-            res.writeHead(200);
-            res.write(search('c') ?? '');
-            res.end();
-        }
-
-        req.resume();
-
-    }).listen(+url.port, url.hostname);
+    return { a, e, d, c };
 
 }
 
-function genLogging ({ debug = false, warn = false } = {}): Logging {
+export function genLogging ({ debug = false, warn = false } = {}): Logging {
 
     return {
 
@@ -330,4 +323,161 @@ function genLogging ({ debug = false, warn = false } = {}): Logging {
     };
 
 }
+
+export const temp = u.run(function () {
+
+    const REQUEST: Result<TE_ES> = ports => ({ url }) => {
+
+        const { headers } = genAuth(url);
+
+        return TE.of(F.tuple(fetchToString(
+            flushHeaders(
+                http.request({
+                    headers,
+                    host: url.hostname,
+                    port: ports.proxy,
+                    method: 'GET',
+                    path: redirect(url, ports.server),
+                }),
+            ),
+        ), ports));
+
+    };
+
+
+
+    const CONNECT: Result<TE_ES> = ports => ({ url }) => {
+
+        const { headers } = genAuth(url);
+
+        return TE.of(F.tuple(F.pipe(
+
+            u.tryCatchToError(() => {
+
+                const req = flushHeaders(
+                    http.request({
+                        headers,
+                        host: url.hostname,
+                        port: ports.proxy,
+                        method: 'CONNECT',
+                        path: R.join(':', [ url.hostname, ports.server ]),
+                    }),
+                );
+
+                return once(req, 'connect');
+
+            }),
+
+            TE.map(R.nth(1)),
+
+            TE.chain((conn: net.Socket) => fetchToString(
+                flushHeaders(
+                    http.request(redirect(url, ports.server), {
+                        createConnection: F.constant(conn),
+                    }),
+                ),
+            )),
+
+        ), ports));
+
+    };
+
+
+
+    const proxy: Result<Closing> = ports => ({ url, logging, flags }) => {
+
+        const trimAuth: typeof genAuth = R.o(
+            genAuth,
+            R.evolve({
+                username: readStr,
+                password: readStr,
+            }),
+        );
+
+        const { auth } = trimAuth(url);
+        const update = R.assoc('proxy', R.__, ports);
+
+        const service: Service = {
+            auth,
+            protocol: R.init(url.protocol) as Service['protocol'],
+            host: url.hostname,
+            port: ports.proxy,
+        };
+
+        return u.tryCatchToError(async () => {
+
+            const { resolve, promise } = Defer<number>();
+
+            const source = box (service) (logging, resolve);
+
+            const dispose = new Rx.Subject<boolean>();
+
+            const close = R.once(() => {
+                dispose.next(true);
+            });
+
+            source.pipe(
+
+                o.mergeMap(({ host, port, hook }) => {
+
+                    if (flags.e) {
+                        return hook();
+                    }
+
+                    return hook(net.connect({ host, port }));
+
+                }),
+
+                o.takeUntil(dispose),
+
+            ).subscribe({ error: close });
+
+            return F.tuple({ close }, update(await promise));
+
+        });
+
+    };
+
+
+
+    const server: Result<Closing> = ports => ({ url, flags }) => {
+
+        const update = R.assoc('server', R.__, ports);
+
+        return TE.fromTask(async () => {
+
+            const { resolve, promise } = Defer<number>();
+
+            const ser = http.createServer((req, res) => {
+
+                if (req.method === 'GET' && req.url === url.pathname) {
+                    res.writeHead(200);
+                    res.write(flags.c ?? '');
+                    res.end();
+                }
+
+                req.resume();
+
+            });
+
+            ser.listen({ port: ports.server, host: url.hostname }, () => {
+
+                const address = ser.address() ?? '';
+                resolve(typeof address === 'string' ? 0 : address.port);
+
+                ser.unref();
+
+            });
+
+            return F.tuple(ser, update(await promise));
+
+        });
+
+    };
+
+
+
+    return { proxy, server, REQUEST, CONNECT };
+
+});
 
