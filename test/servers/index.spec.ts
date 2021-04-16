2619ebadb646165c0acd0f71d7b0f3ab0dcbb42f
Cotton Hou <himcotton@gmail.com>
2021-04-17 03:38:24 +0800

    refine DNS cache alter by IORef

---
+++ test/servers/index.spec.ts
@@ -0,0 +1,76 @@
+import * as R from 'ramda';
+
+import {
+    readonlyMap as M,
+    either as E,
+    option as O,
+    ioRef as Ref,
+} from 'fp-ts';
+
+import * as u from '../../src/utils/index';
+
+import { updateCache } from '../../src/servers/index';
+
+
+
+
+
+describe('updateCache', () => {
+
+    const key = 'key';
+    const val = 'val';
+
+    const init = {
+        timeout: 80,
+        dns: O.none,
+        doh: O.none,
+        dot: O.none,
+        ttl: O.none,
+    };
+
+    test('cached', async () => {
+
+        const cache = u.run(Ref.newIORef(M.singleton(key, val)));
+
+        const task = updateCache (val) (10) ({
+            host: key,
+            resolver: { ...init, cache },
+        });
+
+        const result = await task();
+
+        expect(E.isLeft(result)).toBe(true);
+
+    });
+
+    test('empty', async () => {
+
+        const cache = u.run(Ref.newIORef(M.empty));
+
+        const ttl = {
+            min: 1,
+            max: 5,
+            calc: R.clamp(1, 5),
+        };
+
+        const task = updateCache (val) (10) ({
+            host: key,
+            resolver: { ...init, cache, ttl: O.some(ttl) },
+        });
+
+        jest.useFakeTimers();
+
+        const result = await task();
+
+        expect(E.isRight(result)).toBe(true);
+        expect(cache.read().has(key as never)).toBe(true);
+        expect(cache.read().get(key as never)).toBe(val);
+
+        jest.advanceTimersByTime(ttl.max * 1000);
+
+        expect(cache.read().has(key as never)).toBe(false);
+
+    });
+
+});
+
