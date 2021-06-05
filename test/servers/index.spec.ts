f023fd975cd7b3662290e5e8507e4137731218e7
Cotton Hou <himcotton@gmail.com>
2021-06-06 01:00:16 +0800

    test DNS resolve

--- test/servers/index.spec.ts
+++ test/servers/index.spec.ts
@@ -1,15 +1,154 @@
+import pino from 'pino';
+
 import * as R from 'ramda';
 
 import {
     readonlyMap as M,
+    readonlyNonEmptyArray as NA,
     either as E,
     option as O,
+    task as T,
+    taskEither as TE,
     ioRef as Ref,
+    function as F,
 } from 'fp-ts';
 
 import * as u from '../../src/utils/index';
 
-import { updateCache } from '../../src/servers/index';
+import {
+    resolve,
+    updateCache,
+} from '../../src/servers/index';
+
+
+
+
+
+describe('resolve', () => {
+
+    const init = {
+        abort: u.noop,
+        hook: () => TE.of(u.run(F.constVoid)),
+        logger: pino({
+            base: null,
+            level: 'silent',
+        }),
+        port: 443,
+        host: 'example.com',
+        resolver: {
+            timeout: 10,
+            dns: O.none,
+            doh: O.none,
+            dot: O.none,
+            ttl: O.none,
+            cache: u.run(Ref.newIORef(M.empty)),
+        },
+    };
+
+    test('ip', async () => {
+
+        const host = '1.2.3.4';
+        const opts = { ...init, host };
+
+        F.pipe(
+            await u.run(resolve(opts)),
+            E.match(
+                e => expect(e).toBeUndefined(),
+                ip => expect(ip).toBe(host),
+            ),
+        );
+
+    });
+
+    test('blocked', async () => {
+
+        const opts = { ...init, host: '0.0.0.0' };
+
+        F.pipe(
+            await u.run(resolve(opts)),
+            E.match(
+                e => expect(e.code).toBe('BLOCKED_HOST'),
+                ip => expect(ip).toBeUndefined(),
+            ),
+        );
+
+    });
+
+    test('cached', async () => {
+
+        const address = '192.168.0.1';
+        const cache = u.run(Ref.newIORef(M.singleton(init.host, address)));
+
+        const resolver = { ...init.resolver, cache };
+        const opts = { ...init, resolver };
+
+        F.pipe(
+            await u.run(resolve(opts)),
+            E.match(
+                e => expect(e).toBeUndefined(),
+                ip => expect(ip).toBe(address),
+            ),
+        );
+
+    });
+
+    test('race bailout', async () => {
+
+        F.pipe(
+            await u.run(resolve(init)),
+            E.match(
+                e => expect(e).toBeUndefined(),
+                ip => expect(ip).toBe(init.host),
+            ),
+        );
+
+    });
+
+    test('timeout', async () => {
+
+        const cache = u.run(Ref.newIORef(M.empty));
+
+        const dns = O.some(NA.of(F.constant(F.pipe(
+            TE.left(new Error('late')),
+            T.delay(init.resolver.timeout + 5),
+        ))));
+
+        const resolver = { ...init.resolver, dns, cache };
+        const opts = { ...init, resolver };
+
+        F.pipe(
+            await u.run(resolve(opts)),
+            E.match(
+                e => expect(e).toBeUndefined(),
+                ip => expect(ip).toBe(init.host),
+            ),
+        );
+
+    }, 20);
+
+    test('dns', async () => {
+
+        const address = '1.2.3.4';
+
+        const dns = O.some(NA.of(F.constant(F.pipe(
+            TE.right(NA.of({ address, ttl: 10 / 1000 })),
+            T.delay(init.resolver.timeout - 5),
+        ))));
+
+        const resolver = { ...init.resolver, dns };
+        const opts = { ...init, resolver };
+
+        F.pipe(
+            await u.run(resolve(opts)),
+            E.match(
+                e => expect(e).toBeUndefined(),
+                ip => expect(ip).toBe(address),
+            ),
+        );
+
+    }, 20);
+
+});
 
 
 
