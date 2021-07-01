f6c9fd9df3c4c7872be42c9cd8cdc07060444e1c
Cotton Hou <himcotton@gmail.com>
2021-07-02 06:14:08 +0800

    export cryptoPairsCE from servers/shadowsocks for extra.ts

--- src/extra.ts
+++ src/extra.ts
@@ -5,6 +5,8 @@ export { convert } from './settings/index';
 export { netConnectTo } from './servers/index';
 
 // eslint-disable-next-line deprecation/deprecation
-export { cryptoPairs, cryptoPairsC } from './servers/shadowsocks';
+export { cryptoPairs } from './servers/shadowsocks';
+export { cryptoPairsC, cryptoPairsCE } from './servers/shadowsocks';
+
 export { socks5Proxy } from './services/socks5';
 
