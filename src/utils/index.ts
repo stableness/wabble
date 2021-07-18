002b221798049dc530f4d3d38c00c77957e2f0a0
Cotton Hou <himcotton@gmail.com>
2021-07-18 13:40:44 +0800

    type BufferEncoding for fs.readFile

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -951,11 +951,12 @@ export const incrementLE2 = F.flow(
 
 
 
+type BE = BufferEncoding;
 type TE_E <T> = TE.TaskEither<Error, T>;
 
 export function readFile (filename: PathLike): TE_E<Buffer>;
-export function readFile (filename: PathLike, encoding: string): TE_E<string>;
-export function readFile (filename: PathLike, encoding?: string) {
+export function readFile (filename: PathLike, encoding: BE): TE_E<string>;
+export function readFile (filename: PathLike, encoding?: BE) {
     return tryCatchToError(() => fs.readFile(filename, encoding));
 }
 
