03fe157ad0948b34a594ac782f8e50cc21518fc1
Cotton Hou <himcotton@gmail.com>
2021-08-24 01:22:22 +0800

    http

--- src/servers/http.ts
+++ src/servers/http.ts
@@ -11,12 +11,11 @@ import {
     function as F,
 } from 'fp-ts';
 
-import { logLevel } from '../model.js';
 import type { Http } from '../config.js';
 
 import * as u from '../utils/index.js';
 
-import type { RTE_O_E_V } from './index.js';
+import { RTE_O_E_V, destroyBy, elapsed } from './index.js';
 
 
 
@@ -24,28 +23,15 @@ import type { RTE_O_E_V } from './index.js';
 
 export const chain: u.Fn<Http, RTE_O_E_V> = remote => opts => {
 
-    const { host, port, logger, hook, abort } = opts;
+    const { host, port, hook, abort } = opts;
 
     return F.pipe(
 
         TE.rightIO(() => R.join(':', [ host, port ])),
 
-        TE.apFirst(TE.fromIO(() => {
+        TE.chain(tunnel(remote)),
 
-            if (R.not(logLevel.on.trace)) {
-                return;
-            }
-
-            const merge = R.pick([ 'host', 'port', 'protocol' ]);
-
-            logger
-                .child({ proxy: merge(remote) })
-                .trace('proxy through http')
-            ;
-
-        })),
-
-        TE.chain(u.catchKToError(tunnel(remote))),
+        elapsed(remote, opts),
 
         TE.mapLeft(R.tap(abort)),
 
@@ -59,50 +45,55 @@ export const chain: u.Fn<Http, RTE_O_E_V> = remote => opts => {
 
 
 
-const TIMEOUT = 1000 * 5;
+const timeoutError = new u.ErrorWithCode(
+    'SERVER_SOCKET_TIMEOUT',
+    'http server timeout',
+);
 
-export const tunnel = (opts: Http) => async (path: string) => {
+const race = u.raceTaskByTimeout(1000 * 5, timeoutError);
 
-    const { protocol, host, port, ssl, auth } = opts;
+export const tunnel = (opts: Http) => (path: string) => u.bracket(
 
-    const hasAuth = u.option2B(auth);
+    TE.rightIO(() => {
 
-    const connect = protocol === 'http' ? http.request : https.request;
+        const { protocol, host, port, ssl, auth } = opts;
 
-    const req = connect({
+        const hasAuth = u.option2B(auth);
 
-        host,
-        port,
-        path,
-        rejectUnauthorized: ssl.verify,
-        method: 'CONNECT',
-        headers: {
+        const headers = {
             'Proxy-Connection': 'Keep-Alive',
-            ...(hasAuth && { 'Proxy-Authorization': authToCredentials(auth) } ),
-        },
-    });
+            ...(hasAuth && { 'Proxy-Authorization': authToCredentials(auth) }),
+        };
 
-    req.setNoDelay(true);
-    req.setTimeout(TIMEOUT);
-    req.setSocketKeepAlive(true, 1000 * 60);
+        const connect = protocol === 'http' ? http.request : https.request;
 
-    req.flushHeaders();
+        return connect({
 
-    try {
+            host,
+            port,
+            path,
+            headers,
+            method: 'CONNECT',
+            rejectUnauthorized: ssl.verify,
 
-        await Promise.race([
-            once(req, 'connect'),
-            u.timeout(TIMEOUT),
-        ]);
+        });
 
-    } catch (err) {
-        req.abort();
-        throw err;
-    }
+    }),
 
-    return req.socket;
+    req => race(u.tryCatchToError(async () => {
 
-};
+        req.setNoDelay(true);
+        req.flushHeaders();
+
+        await once(req, 'connect');
+
+        return req.socket;
+
+    })),
+
+    destroyBy(timeoutError),
+
+);
 
 
 
