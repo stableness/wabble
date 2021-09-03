1ac94910be1b62f062dc057c7c091f0a201620f0
Cotton Hou <himcotton@gmail.com>
2021-09-03 21:16:36 +0800

    curries

--- src/utils/curries.ts
+++ src/utils/curries.ts
@@ -2,9 +2,13 @@ import { function as F } from 'fp-ts-std';
 
 const {
     curry2, curry3, curry4, curry5,
+    curry2T, curry3T, curry4T, curry5T,
+    uncurry2, uncurry3, uncurry4, uncurry5,
 } = F;
 
 export {
     curry2, curry3, curry4, curry5,
+    curry2T, curry3T, curry4T, curry5T,
+    uncurry2, uncurry3, uncurry4, uncurry5,
 };
 
